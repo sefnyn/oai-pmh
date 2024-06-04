2b737916c729a378112aa963b56d27ea0e101f84
Nicholas Syrotiuk <sefnyn@gmail.com>
2024-06-04 16:54:45 +0100

    successful test on small sample

--- get-doi.py
+++ get-doi.py
@@ -1,15 +1,17 @@
 import sys
-import xml.dom.minidom
+from xml.dom import minidom
 from sickle import Sickle
+import xml.etree.ElementTree as ET
 
 ENDPOINT = 'https://durham-repository.worktribe.com/oaiprovider'
 sickle = Sickle(ENDPOINT)
 
 prefix = 'oai:durham-repository.worktribe.com:'
 log    = 'logfile.log'
+doi    = 'doi.txt'
 
 fh1 = open(log, 'w')
-
+fh2 = open(doi, 'w')
 
 def get_rioxx(repo_id):
     longid = prefix + repo_id
@@ -17,21 +19,29 @@ def get_rioxx(repo_id):
         r = sickle.GetRecord(identifier=longid, metadataPrefix='rioxx')
 #        print("Got record " + repo_id + " in rioxx format")
         pp = pprint(r)
-        fh1.write(pp)
         return pp
     except:
         print("Skipping invalid output ID")
-        fh2.write("Skipping invalid output ID " + repo_id + "\n")
+        fh1.write("Skipping invalid output ID " + repo_id + "\n")
         return None
 
 def analyse(r, id, ctr):
     if '<rioxxterms:version_of_record>' in r:
         ctr['doi'] += 1
+        root = ET.fromstring(r)
+#        for elem in root.iter():
+#            print(elem.tag)
+
+#        print('rioxxterms:version_of_record')
+        for doi in root.iter('{http://docs.rioxx.net/schema/v2.0/rioxxterms/}version_of_record'):
+#            print(doi.text)
+            fh2.write(doi.text + "\n")
+
     ctr['total'] += 1
 
 
 def pprint(x):
-    dom = xml.dom.minidom.parseString(str(x))
+    dom = minidom.parseString(str(x))
     pretty = dom.toprettyxml()
 #    print(pretty) #for testing
     return pretty
@@ -53,6 +63,7 @@ def main():
     except IndexError:
         print("Usage:\n", sys.argv[0], "FileContainingWorktribeOutputIDs")
     fh1.close()
+    fh2.close()
 
 
 if __name__ == '__main__':
